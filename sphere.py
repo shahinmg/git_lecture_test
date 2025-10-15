acb9f1244fb296272d55a20cfa7b35b8cf38be9f
shahinmg <shahinmg@ku.edu>
2025-10-15 16:11:22 -0500

    Revert "wrong function"
    This reverts commit ed9e485ef41364ed69882459beab27d1fbf339cc.

--- sphere.py
+++ sphere.py
@@ -8,12 +8,3 @@ def sphere_volume(radius):
   volume = (4/3) * math.pi * (radius ** 3)
   
   return volume
-
-def sphere_volume_wrong(radius):
-  
-  # if radius < 0:
-  #   raise ValueError("Radius cannot be negative.")
-    
-  volume = (4/3) * math.pi * (radius ** 3)
-  
-  return volume
